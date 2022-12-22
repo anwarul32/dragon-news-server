cdba6e928adb7b09d58a826deccb12165d20e4ae
anwarul <anwarulhoque032@gmail.com>
2022-12-22 20:04:53 +0600

    added news

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const port =  process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
-    res.send('News API Running..')
+    res.send('News API Running..');
 })
 
 app.get('/news-categories', (req, res) => {
-    res.send(categories)
+    res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const newsForCategories = news.filter(n => n.category_id === id);
+        res.send(newsForCategories);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    // const id = parseINt(req.params.id);
+    const id = req.params.id;
+    const selectNews = news.find(n => n._id === id);
+    res.send(selectNews);
 })
 
 
